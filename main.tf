bc77f9e9d1b2b0c1ffacbd63c48bf179c2dd156f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-06 01:24:03 +0900

    feat: added sql instance

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ provider "google" {
   project     = "${lookup(var.project_name, "${terraform.workspace}")}"
   region      = "asia-northeast1"
 }
+
+resource "google_app_engine_application" "app" {
+  project     = "${lookup(var.project_name, "${terraform.workspace}")}"
+  location_id = "${lookup(var.location, "${terraform.workspace}")}"
+}
+
+resource "google_sql_database_instance" "master" {
+  name             = "master-instance"
+  database_version = "POSTGRES_9_6"
+  region           = "${lookup(var.location, "${terraform.workspace}")}"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
